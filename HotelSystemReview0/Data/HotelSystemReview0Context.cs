e96b5d403d3932be2a49a01241edd77e7fa15b3a
Nathan Shuai <nathan.shuai@gmail.com>
2023-09-08 17:50:55 -0500

    Add project files.

---
+++ HotelSystemReview0/Data/HotelSystemReview0Context.cs
@@ -0,0 +1,16 @@
+﻿using HotelSystemReview0.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelSystemReview0.Data
+{
+    public class HotelSystemReview0Context: DbContext
+    {
+        public HotelSystemReview0Context(DbContextOptions<HotelSystemReview0Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Client { get; set; } = default!;
+        public DbSet<Room> Room { get; set; } = default!;
+    }
+}
